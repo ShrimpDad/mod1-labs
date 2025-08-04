a40d970db052fef896e321d724f2b5492da1f555
ShrimpDad <ckdatahub@gmail.com>
2025-08-04 15:35:16 +0100

    lab 02 completed

---
+++ src/labs/lab2.java
@@ -0,0 +1,33 @@
+package labs;
+
+public class lab2 {
+
+	public static void main(String[] args) {
+		int mark = 5220;
+		System.out.println( getGrade( mark ) );
+		System.out.printf("For your mark %d/100 grade is: %s", mark, getGrade( mark ) );
+	}
+	
+	public static String getGrade ( int mark ) {
+		if ( mark < 0 || mark > 100 ) {
+			return "Error: marks must be between 1..100";
+		}
+
+		String result = "";
+		
+		if ( mark < 50 ) {
+			result = "Fail";
+		}
+		else if ( (mark >= 50) && ( mark <= 60 ) ) {
+			result = "Pass";
+		}
+		else if ( (mark >= 61) && ( mark <= 70 ) ) {
+			result = "Merit";
+		}
+		else if ( (mark >= 71) && ( mark <= 100 ) ) {
+			result = "Distinction";
+		}
+		
+		return result;
+	}
+}
